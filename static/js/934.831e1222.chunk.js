e9791c6eea906e5945f27084659709db86a16749
andrewfriday13 <110158654+andrewfriday13@users.noreply.github.com>
2023-04-20 08:08:44 +0000

    Deploying to gh-pages from @ andrewfriday13/goit-react-hw-08-phonebook@59d6559ae8d9d25b94562aa83fd664eac1aa7a4e 🚀

---
+++ static/js/934.831e1222.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[934],{3934:function(e,n,h){h.r(n);var o=h(184);n.default=function(){return(0,o.jsx)("div",{children:(0,o.jsx)("h1",{children:"Main page"})})}}}]);
+//# sourceMappingURL=934.831e1222.chunk.js.map
